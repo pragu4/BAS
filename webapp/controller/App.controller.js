926b5df783869268a46f74547640db5ada8da5cd
pragnesh <pragneshdineshbhai@.com>
2022-11-02 01:35:11 +0000

    master init

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("bas.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
